6b93733193dc2f19ba65564ee7edc58ddd5bc5e0
Sim kaur <moblue2012@gmail.com>
2020-12-02 00:22:29 -0500

    add

--- develop/index.js
+++ develop/index.js
@@ -19,7 +19,7 @@ inq.prompt ([
     {
         type:'input',
         message :"Table of Contents",
-         name: "list" 
+        name: "checkbox" 
     },
     // WHEN I click on the links in the Table of Contents, THEN I am taken to the corresponding section of the README
     {
